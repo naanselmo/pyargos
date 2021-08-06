febf0c7338148cda59410915058a629e7fadda3a
Nuno Anselmo <me@nunoanselmo.me>
2021-08-06 23:45:18 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyargos",
-    version="0.0.1",
+    version="0.0.2",
     author="Nuno Anselmo",
     author_email="pyargos@nunoanselmo.me",
     description="A Python library for easier interfacing with Argos",
